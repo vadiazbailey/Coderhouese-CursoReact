77c781d7bac96d542dc509b66f4399e24e3fdf99
vadiazbailey <vdiazbailey@alumnos.exa.unicen.edu.ar>
2023-10-11 18:09:43 -0300

    Nav ba

--- afrodita/src/App.js
+++ afrodita/src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
+import NavBar from './components/NavBar/NavBar';
+import Fondo from './assets/fondo.jpg';
 
 function App() {
   return (
     <div className="App">
-      
+      <NavBar />
+      <div className='imagenHome'>
+        <img src={Fondo} alt='afrodita' />
+      </div>
     </div>
   );
 }
